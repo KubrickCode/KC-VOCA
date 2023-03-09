dfb1448782875ba72de2c760b39f6d0b7e0429be
KubrickCode <kubrickcode@gmail.com>
2023-03-09 21:02:39 +0900

    againREST

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
     return savedTheme || "light";
   });
 
-  const url = "/api";
+  const url = "http://localhost:3000/api";
 
   useEffect(() => {
     const fetchIsLogin = async () => {
