01324e324f1f46ecbb4127719c149ebd8dd64156
KubrickCode <kubrickcode@gmail.com>
2023-04-03 22:35:28 +0900

    no minify

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: { chunkSizeWarningLimit: 1600, minify: false },
+  build: { chunkSizeWarningLimit: 1600 },
   plugins: [react()],
 });
